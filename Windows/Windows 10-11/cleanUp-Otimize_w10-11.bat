99aa010733ec8a3a186570900d09b34a8521ac28
@thangminh999 <79648170+thangminh999@users.noreply.github.com>
2023-06-13 13:21:56 +0700

    update_13-06-2023_1:21PM

---
+++ Windows/Windows 10-11/cleanUp-Otimize_w10-11.bat
@@ -0,0 +1,60 @@
+@echo off
+REM Cleaning up and optimizing the system
+REM (C) 2023 @thangminh999. All rights reserved.
+echo Cleaning up and optimizing the system...
+echo.
+
+:: 1
+echo Deleting temporary files in the %Temp% folder...
+echo Wait for 2 seconds
+timeout /t 2 >nul
+del /f /q "%Temp%\*.*" >nul 2>&1
+
+:: 2
+echo Deleting temporary files in the %SystemRoot%\Temp folder...
+echo Wait for 2 seconds
+timeout /t 2 >nul
+del /f /q "%SystemRoot%\Temp\*.*" >nul 2>&1
+
+:: 3
+echo Deleting temporary files in the Prefetch folder...
+echo Wait for 2 seconds
+timeout /t 2 >nul
+del /s /f /q C:\Windows\Prefetch\*.*
+
+:: 4
+echo Deleting temporary files in the Downloads folder...
+echo Wait for 2 seconds
+timeout /t 2 >nul
+del /s /q "C:\Windows\SoftwareDistribution\Download\*"
+
+:: 5
+echo Emptying the recycle bin...
+echo Wait for 2 seconds
+timeout /t 2 >nul
+PowerShell.exe -NoProfile -Command Clear-RecycleBin -Confirm:$false
+
+echo Wait for 2 seconds before clearing browsing history!!!
+timeout /t 2 >nul
+:: 6
+echo Clearing browsing history in Internet Explorer...
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255 >nul 2>&1
+echo Clearing browsing history in Microsoft Edge...
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1023 >nul 2>&1
+echo Clearing browsing history in Firefox...
+del /f /q "%LocalAppData%\Mozilla\Firefox\Profiles\*.default\places.sqlite" >nul 2>&1
+echo Clearing browsing history in Google Chrome...
+del /f /q "%LocalAppData%\Google\Chrome\User Data\Default\History" >nul 2>&1
+
+echo Wait for 2 seconds for registry optimization!!!
+timeout /t 2 >nul
+
+:: 7
+echo Optimizing the registry...
+reg.exe ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Active Setup Temp Folders" /v StateFlags0061 /t REG_DWORD /d 2 /f >nul 2>&1
+reg.exe ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Thumbnail Cache" /v StateFlags0111 /t REG_DWORD /d 2 /f >nul 2>&1
+reg.exe ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Temporary Files" /v StateFlags0111 /t REG_DWORD /d 2 /f >nul 2>&1
+
+echo ====================================================================
+echo Completed! The system has been cleaned up and optimized.
+pause
--- Windows/Windows 10/test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM (C) 2023 @thangminh999. All rights reserved.
-echo.
-echo comming soon...
-pause
